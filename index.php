9997610e7679196515cb22291d8a8969c1a5a431
Kavitha Suresh Kumar <kavisuresh@in.ibm.com>
2020-05-04 22:27:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
-<!DOCTYPE html>
 <html>
-<head>
-<title>Kavitha Suresh Kumar - Rock Paper Scissors</title>
-<?php require_once "bootstrap.php"; ?>
-</head>
+<title>Kavitha Suresh Kumar PHP</title>
 <body>
-<div class="container">
-<h1>Welcome to Rock Paper Scissors</h1>
-<p>
-<a href="login.php">Please Log In</a>
+<h1>Kavitha Suresh Kumar PHP</h1>
+<pre>ASCII ART:
+
+   *     *
+   *   *
+   * *
+   *
+   * *
+   *   *
+   *     *
+
+</pre>
+<p>The SHA256 hash of Kavitha Suresh Kumar is
+<?php
+print hash('sha256', 'Kavitha Suresh Kumar');
+?>
 </p>
-<p>
-Attempt to go to
-<a href="game.php">game.php</a> without logging in - it should fail with an error message.
-</div>
 </body>
+<a href="fail.php">Click here to see error messages</a>
+</br>
+<a href="check.php">Click here to see the condition checks</a>
+</html>
